baf4505b39c25d9e5d007422ce34f66e56601f6f
Danilo Ferreira <54878348+danilojf@users.noreply.github.com>
2026-09-06 20:12:38 -0300

    Add files via upload

---
+++ application/MediaAluno.java
@@ -0,0 +1,17 @@
+package application;
+
+public class MediaAluno {
+
+    public static void main(String[] args) {
+        double nota1 = 5.4;
+        double nota2 = 7.0;
+        double nota3 = 7.5;
+        double nota4 = 8.4;
+        double mediaNotasAlunos = nota1 + nota2 + nota3 + nota4;
+        System.out.println("Primeira nota:" + nota1);
+        System.out.println("Segunda nota:" + nota2);
+        System.out.println("Terceira nota:" + nota3);
+        System.out.println("Quarta nota:" + nota4);
+        System.out.println("Resultado Média: " + mediaNotasAlunos);
+    }
+}
